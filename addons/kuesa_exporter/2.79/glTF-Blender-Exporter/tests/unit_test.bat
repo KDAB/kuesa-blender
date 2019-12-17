b588bfb3169f82b100fb4fe60c0cb08baac51cb5
Juan Jose Casafranca <juan.casafranca@kdab.com>
2019-12-17 13:02:44 +0100

    Remove kuesa_exporter
    Change-Id: I51d1f59df4a786b9705d1b7d6c8fd640bbb2b235

--- addons/kuesa_exporter/2.79/glTF-Blender-Exporter/tests/unit_test.bat
+++
@@ -1,1 +0,0 @@
-"C:/Program Files/Blender Foundation/Blender/blender.exe" -b -P unit_test.py -- %cd%
--- addons/kuesa_exporter/2.80/glTF-Blender-IO-master/docs/README.md
+++
@@ -1,3 +0,0 @@
-# Documentation
-
-See: https://docs.blender.org/manual/en/dev/addons/io_gltf2.html
